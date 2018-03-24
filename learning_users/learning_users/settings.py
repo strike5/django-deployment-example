94c71b98a6bb27703d422d38c50b09bd4ad17094
Johann Cruz <jh.cruz91@gmail.com>
2018-03-23 20:00:35 -0400

    Allowing Host

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'x3p(uys)&6vixgiwnh4h45sf_*ec&_6vg3cm^6ik$_!eerv$4@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://strike5.pythonanywhere.com/']
 
 
 # Application definition
